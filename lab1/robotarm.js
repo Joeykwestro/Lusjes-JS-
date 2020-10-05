2c43d7f1148d6e2cf0fd3b12f9ef91b4a35c2b39
Joey Kwestro <99062903@mydavinci.nl>
2020-10-05 11:13:49 +0200

    .

---
+++ lab1/robotarm.js
@@ -0,0 +1,8 @@
+  var canvas = document.getElementById("canvas");
+  var robotArm = new RobotArm(canvas);
+
+  robotArm.loadLevel("exercise 1");
+
+  // Je eigen code plaats je hier.
+
+  robotArm.run();
